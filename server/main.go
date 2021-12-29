dc4b7235417375ca6ae03bba9cd4f280211559de
Andey Robins <andeyrobins@gmail.com>
2021-12-29 13:59:46 -0700

    Build page login

--- server/main.go
+++ server/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -9,7 +11,24 @@ import (
 
 func main() {
 	http.HandleFunc("/register", auth.Register)
-	http.HandleFunc("/login", auth.Login)
+	http.HandleFunc("/login", testLogin)
 
+	log.Println("Starting go server on 8080")
 	log.Fatalln(http.ListenAndServe(":8080", nil))
+}
+
+func testLogin(w http.ResponseWriter, r *http.Request) {
+	response, err := getTestJson()
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	fmt.Fprint(w, string(response))
+}
+
+func getTestJson() ([]byte, error) {
+	j := make(map[string]string)
+	j["token"] = "token123"
+	return json.MarshalIndent(j, "", "  ")
 }
